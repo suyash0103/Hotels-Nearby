1f978648ff9c16d8db0a52996a6f68db0d57b595
suyash0103 <suyashghuge@gmail.com>
2018-04-05 14:29:46 +0530

    owner logout

--- Hotels/app/src/main/java/com/example/suyash/hotelsnearby/Owner.java
+++ Hotels/app/src/main/java/com/example/suyash/hotelsnearby/Owner.java
@@ -1,7 +1,16 @@
 package com.example.suyash.hotelsnearby;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class Owner extends AppCompatActivity {
 
@@ -9,5 +18,34 @@ public class Owner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void signOut() {
+        MainActivity.mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(Owner.this, "Logged Out", Toast.LENGTH_SHORT).show();
+                        Intent AfterLoginIntent = new Intent(Owner.this, MainActivity.class);
+                        startActivity(AfterLoginIntent);
+                        finish();
+                    }
+                });
     }
 }
