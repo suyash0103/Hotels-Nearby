7058ac8f79945baf732f8392086b4c518e2a91d5
suyash0103 <suyashghuge@gmail.com>
2018-04-07 12:04:21 +0530

    new activity

--- Hotels/app/src/main/java/com/example/suyash/hotelsnearby/AddHotel.java
+++ Hotels/app/src/main/java/com/example/suyash/hotelsnearby/AddHotel.java
@@ -2,12 +2,25 @@ package com.example.suyash.hotelsnearby;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddHotel extends AppCompatActivity {
+
+    EditText name, address, open_time, close_time;
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_hotel);
+
+        name = (EditText) findViewById(R.id.name);
+        address = (EditText) findViewById(R.id.address);
+        open_time = (EditText) findViewById(R.id.open_time);
+        close_time = (EditText) findViewById(R.id.close_time);
+
+        save = (Button) findViewById(R.id.save);
+
     }
 }
